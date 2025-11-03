0b0f79a1c6d02668225d45be7fd3c8b4e476fde3
GursimranKaur97 <gursimrankaur1664@gmail.com>
2025-11-03 10:56:28 +0530

    redirect to login if not authenticated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addFeed } from "../utils/feedSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
 
-export default Feed
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const response = await axios.get(BASE_URL + "/user/feed", { withCredentials: true });
+      dispatch(addFeed(response.data?.data));
+      console.log(response?.data?.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {   
+    getFeed();
+  }, [])
+
+  return <div>Feed</div>;
+};
+
+export default Feed;
